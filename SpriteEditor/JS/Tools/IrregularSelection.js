4e4959bce685ddd76d9a589568eeaecbf769090b
MarkusArtemov <Martemov16@gmail.com>
2024-07-27 23:32:03 +0200

    Add the same logic to IrregularSelection

--- SpriteEditor/JS/Tools/IrregularSelection.js
+++ SpriteEditor/JS/Tools/IrregularSelection.js
@@ -1,7 +1,6 @@
 import { SpriteEditor } from "../Elements/SpriteEditor.js";
 import { SelectionTool } from "./SelectionTool.js";
 
-
 export class IrregularSelection extends SelectionTool {
   /**
    * @param {SpriteEditor} sprite_editor
@@ -13,44 +12,43 @@ export class IrregularSelection extends SelectionTool {
     this.is_drawing = false;
     this.is_moving = false;
     this.has_moved = false;
+    this.stopped_drawing = false;
   }
 
   /**
-   * @param {MouseEvent} event 
+   * @param {MouseEvent} event
    */
   mouse_down(event) {
     if (!this.mouse_over_selected_area(event)) {
       if (this.path.length > 0) {
         this.path = [];
       }
-      this.pending_drawing = true;
+      if (this.stopped_drawing) {
+        this.sprite_editor.destroy_selection();
+        this.stopped_drawing = false;
+      }
+      this.is_drawing = true;
       const position = this.get_mouse_position(event);
       this.path = [position];
       this.sprite_editor.set_selection_start_point(position);
       this.draw(event);
-
     } else {
-      this.sprite_editor.set_selection_move_start_point(this.get_mouse_position(event));
+      this.sprite_editor.set_selection_move_start_point(
+        this.get_mouse_position(event)
+      );
       this.is_moving = true;
     }
   }
 
   /**
-   * @param {MouseEvent} event 
+   * @param {MouseEvent} event
    */
   mouse_move(event) {
     if (this.is_moving) {
       this.move(event);
       this.has_moved = true;
       return;
-    } 
-    if (this.pending_drawing) {
-      this.is_drawing = true;
-      this.pending_drawing = false;
-      this.add_point_to_path(event);
-      this.draw(event);
-      return;
-    } 
+    }
     if (this.is_drawing) {
       this.add_point_to_path(event);
       this.draw(event);
@@ -58,30 +56,27 @@ export class IrregularSelection extends SelectionTool {
   }
 
   /**
-   * @param {MouseEvent} event 
+   * @param {MouseEvent} event
    */
   mouse_up(event) {
     if (this.is_moving) {
       this.is_moving = false;
       this.has_moved = false;
       return;
-    } 
+    }
     if (this.is_drawing) {
       this.is_drawing = false;
+      this.stopped_drawing = true;
       this.path = this.sprite_editor.selected_points;
       const selectedPoints = this.find_points_inside_path();
       this.path.concat(selectedPoints);
       this.sprite_editor.fill_selection(selectedPoints);
       return;
-    } 
-    if (this.pending_drawing) {
-      this.pending_drawing = false;
-      this.path = [];
     }
   }
 
   /**
-   * @param {MouseEvent} event 
+   * @param {MouseEvent} event
    */
   add_point_to_path(event) {
     const position = this.get_mouse_position(event);
@@ -89,14 +84,14 @@ export class IrregularSelection extends SelectionTool {
   }
 
   /**
-   * @param {MouseEvent} event 
+   * @param {MouseEvent} event
    */
   draw(event) {
     this.sprite_editor.draw_lasso_selection(this.path);
   }
 
   /**
-   * @param {MouseEvent} event 
+   * @param {MouseEvent} event
    */
   move(event) {
     const position = this.get_mouse_position(event);
@@ -108,7 +103,7 @@ export class IrregularSelection extends SelectionTool {
 
   /**
    * Finds all points inside the current selection path using a bounding box approach.
-   * 
+   *
    * @returns {Array<{x: Number, y: Number}>}
    */
   find_points_inside_path() {
@@ -128,11 +123,11 @@ export class IrregularSelection extends SelectionTool {
     return pointsInside;
   }
 
-    /**
+  /**
    * Checks if a point is inside the path defined by path using the ray-casting algorithm.
-   * 
-   * @param {{ x: number, y: number }} point 
-   * @returns {boolean} 
+   *
+   * @param {{ x: number, y: number }} point
+   * @returns {boolean}
    */
   is_point_inside_path(point) {
     const px = point.x;
@@ -145,14 +140,15 @@ export class IrregularSelection extends SelectionTool {
       const pxj = this.path[j].x;
       const pyj = this.path[j].y;
 
-      const is_above = (pyi > py) !== (pyj > py);
-      const is_left_of_edge = px < (pxj - pxi) * (py - pyi) / (pyj - pyi) + pxi;
+      const is_above = pyi > py !== pyj > py;
+      const is_left_of_edge =
+        px < ((pxj - pxi) * (py - pyi)) / (pyj - pyi) + pxi;
 
       if (is_above && is_left_of_edge) {
         inside = !inside;
       }
-  }
+    }
 
-  return inside;
-}
+    return inside;
+  }
 }
