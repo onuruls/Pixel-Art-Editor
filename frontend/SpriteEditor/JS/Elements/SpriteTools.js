3f8387e33082b4d05e4caf876e11dc69d4a87b95
onuruls <onur-uls@hotmail.com>
2024-09-09 16:14:02 +0200

    Add: button hover info, mirror pen both axis

--- frontend/SpriteEditor/JS/Elements/SpriteTools.js
+++ frontend/SpriteEditor/JS/Elements/SpriteTools.js
@@ -1,4 +1,5 @@
 import { SpriteEditorPart } from "./SpriteEditorPart.js";
+import { Util } from "../../../Util/Util.js";
 
 export class SpriteTools extends SpriteEditorPart {
   constructor(sprite_editor) {
@@ -9,20 +10,21 @@ export class SpriteTools extends SpriteEditorPart {
     const palette_html = this.sprite_editor.palettes
       .map(
         (color, i) => `
-      <input type="color" class="palette-color" data-index="${i}" value="${color}">
-    `
+        <input type="color" class="palette-color" data-index="${i}" value="${color}" 
+        data-info='[" Color palette", "SHIFT to set palette to primary color", "CTRL to open color selector"]'>
+      `
       )
       .join("");
     return `
     <div class="pixel-size-options" title="Pixel size">
-      <button class="pixel-size-option active" id="size_1" data-size="1">
-      <div class="bg"/>
+      <button class="pixel-size-option active" id="size_1" data-size="1" data-info='[" 1px size"]'>
+        <div class="bg"/>
       </button>
-      <button class="pixel-size-option" id="size_2" data-size="2">
-      <div class="bg"/>
+      <button class="pixel-size-option" id="size_2" data-size="2" data-info='[" 2px size"]'>
+        <div class="bg"/>
       </button>
-      <button class="pixel-size-option" id="size_3" data-size="4">
-      <div class="bg"/>
+      <button class="pixel-size-option" id="size_3" data-size="4" data-info='[" 4px size"]'>
+        <div class="bg"/>
       </button>
     </div>
 
@@ -31,7 +33,7 @@ export class SpriteTools extends SpriteEditorPart {
         id="pen"
         class="tool-button active"
         data-tool="pen"
-        title="Pen tool (P)"
+        data-info='["(P) Pen tool"]'
       >
         <img src="img/icons/pen.svg" alt="Pen" />
       </button>
@@ -39,7 +41,7 @@ export class SpriteTools extends SpriteEditorPart {
         id="mirror_pen"
         class="tool-button"
         data-tool="mirror_pen"
-        title="Mirror pen tool (M)"
+        data-info='["(M) Mirror Pen", "SHIFT for horizontal axis", "CTRL for both axis"]'
       >
         <img src="img/icons/mirror_pen.svg" alt="Mirror Pen" />
       </button>
@@ -47,7 +49,7 @@ export class SpriteTools extends SpriteEditorPart {
         id="bucket"
         class="tool-button"
         data-tool="bucket"
-        title="Paint bucket tool (B)"
+        data-info='["(B) Bucket tool"]'
       >
         <img src="img/icons/bucket.svg" alt="Bucket" />
       </button>
@@ -55,7 +57,7 @@ export class SpriteTools extends SpriteEditorPart {
         id="same_color"
         class="tool-button"
         data-tool="same_color"
-        title="Paint all pixels of the same color (C)"
+        data-info='["(N) Paint all pixels of same color"]'
       >
         <img src="img/icons/same_color_bucket.svg" alt="Same Color Bucket" />
       </button>
@@ -63,7 +65,7 @@ export class SpriteTools extends SpriteEditorPart {
         id="eraser"
         class="tool-button"
         data-tool="eraser"
-        title="Eraser tool (E)"
+        data-info='["(E) Eraser"]'
       >
         <img src="img/icons/eraser.svg" alt="Eraser" />
       </button>
@@ -71,7 +73,7 @@ export class SpriteTools extends SpriteEditorPart {
         id="stroke"
         class="tool-button"
         data-tool="stroke"
-        title="Stroke tool (S)"
+        data-info='["(S) Stroke", "SHIFT for straight line"]'
       >
         <img src="img/icons/stroke.svg" alt="Stroke" />
       </button>
@@ -79,7 +81,7 @@ export class SpriteTools extends SpriteEditorPart {
         id="rectangle"
         class="tool-button"
         data-tool="rectangle"
-        title="Rectangle tool (R)"
+        data-info='["(R) Rectangle tool", "SHIFT for square"]'
       >
         <img src="img/icons/rectangle.svg" alt="Rectangle" />
       </button>
@@ -87,7 +89,7 @@ export class SpriteTools extends SpriteEditorPart {
         id="circle"
         class="tool-button"
         data-tool="circle"
-        title="Circle tool (O)"
+        data-info='["(C) Circle tool", "SHIFT for circle"]'
       >
         <img src="img/icons/circle.svg" alt="Circle" />
       </button>
@@ -95,7 +97,7 @@ export class SpriteTools extends SpriteEditorPart {
         id="move"
         class="tool-button"
         data-tool="move"
-        title="Move tool (V)"
+        data-info='["(V) Move tool"]'
       >
         <img src="img/icons/move.svg" alt="Move" />
       </button>
@@ -103,7 +105,7 @@ export class SpriteTools extends SpriteEditorPart {
         id="shape_selection"
         class="tool-button"
         data-tool="shape_selection"
-        title="Shape Selection (L)"
+        data-info='["(Q) Shape Selection"]'
       >
         <img src="img/icons/fill_shape.svg" alt="Shape" />
       </button>
@@ -111,7 +113,7 @@ export class SpriteTools extends SpriteEditorPart {
         id="rectangle_selection"
         class="tool-button"
         data-tool="rectangle_selection"
-        title="Rectangle Selection (T)"
+        data-info='["(T) Rectangle Selection"]'
       >
         <img src="img/icons/rectangle_selection.svg" alt="Rectangle Selection" />
       </button>
@@ -119,7 +121,7 @@ export class SpriteTools extends SpriteEditorPart {
         id="irregular_selection"
         class="tool-button"
         data-tool="irregular_selection"
-        title="Irregular Selection (I)"
+        data-info='["(I) Irregular Selection"]'
       >
         <img
           src="img/icons/irregular_selection.svg"
@@ -130,7 +132,7 @@ export class SpriteTools extends SpriteEditorPart {
         id="lighting"
         class="tool-button"
         data-tool="lighting"
-        title="Lighten (H)"
+        data-info='["(L) Lighten", "SHIFT for darken"]'
       >
         <img src="img/icons/lighting.svg" alt="Lighting" />
       </button>
@@ -138,7 +140,7 @@ export class SpriteTools extends SpriteEditorPart {
         id="dithering"
         class="tool-button"
         data-tool="dithering"
-        title="Dithering (D)"
+        data-info='["(D) Dithering"]'
       >
         <img src="img/icons/dithering.svg" alt="Dithering" />
       </button>
@@ -146,34 +148,33 @@ export class SpriteTools extends SpriteEditorPart {
         id="color_picker"
         class="tool-button"
         data-tool="color_picker"
-        title="Color Picker (K)"
+        data-info='["(O) Color Picker"]'
       >
         <img src="img/icons/color_picker.svg" alt="Color Picker" />
       </button>
     </div>
     <div class="color-selection">
       <input
-          type="color"
-          class="color-input"
-          id="color_input"
-          name="color_input"
-          value="#000000"
+        type="color"
+        class="color-input"
+        id="color_input"
+        name="color_input"
+        value="#000000"
+        data-info='[" Primary color - left mouse button"]'
       />
       <button class="swap-btn">
         <i class="fa-solid fa-right-left"></i>
       </button>
       <input
-          type="color"
-          class="color-input"
-          id="secondary_color_input"
-          name="secondary_color_input"
-          value="#ffffff"
+        type="color"
+        class="color-input"
+        id="secondary_color_input"
+        name="secondary_color_input"
+        value="#ffffff"
+        data-info='[" Secondary color - right mouse button"]'
       />
     </div>
-    <div class="palettes" title=
-    "Color palettes 
-+ SHIFT to set palette to primary color
-+ CTRL to open color selector">
+    <div class="palettes">
       ${palette_html}
     </div>
     `;
@@ -182,6 +183,10 @@ export class SpriteTools extends SpriteEditorPart {
   init() {
     const tool_buttons = document.querySelectorAll(".tool-button");
     tool_buttons.forEach((button) => {
+      const tool_info_string = button.getAttribute("data-info");
+      const tool_info = JSON.parse(tool_info_string);
+      Util.create_tool_info(button, tool_info);
+
       button.addEventListener("click", () => {
         tool_buttons.forEach((btn) => btn.classList.remove("active"));
         button.classList.add("active");
@@ -190,6 +195,9 @@ export class SpriteTools extends SpriteEditorPart {
 
     const pixel_size_options = document.querySelectorAll(".pixel-size-option");
     pixel_size_options.forEach((button) => {
+      const pixel_info_string = button.getAttribute("data-info");
+      const pixel_info = JSON.parse(pixel_info_string);
+      Util.create_tool_info(button, pixel_info);
       button.addEventListener("click", () => {
         pixel_size_options.forEach((btn) => btn.classList.remove("active"));
         button.classList.add("active");
@@ -199,6 +207,9 @@ export class SpriteTools extends SpriteEditorPart {
 
     const palette_colors = this.querySelectorAll(".palette-color");
     palette_colors.forEach((palette) => {
+      const palette_info_string = palette.getAttribute("data-info");
+      const palette_info = JSON.parse(palette_info_string);
+      Util.create_tool_info(palette, palette_info);
       palette.addEventListener("click", (event) => {
         const index = palette.getAttribute("data-index");
         if (event.ctrlKey) {
@@ -235,6 +246,19 @@ export class SpriteTools extends SpriteEditorPart {
     const secondary_color_input = document.querySelector(
       "#secondary_color_input"
     );
+
+    const primary_color_info = JSON.parse(
+      primary_color_input.getAttribute("data-info")
+    );
+    primary_color_info.push(` ${primary_color_input.value}`);
+
+    const secondary_color_info = JSON.parse(
+      secondary_color_input.getAttribute("data-info")
+    );
+    secondary_color_info.push(` ${secondary_color_input.value}`);
+
+    Util.create_tool_info(primary_color_input, primary_color_info);
+    Util.create_tool_info(secondary_color_input, secondary_color_info);
 
     swap_button.addEventListener("click", () => {
       const primary_color = primary_color_input.value;
