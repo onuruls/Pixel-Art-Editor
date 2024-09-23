9c381f4847ac615caf4e9e1672ca5b936aa2af77
MarkusArtemov <Martemov16@gmail.com>
2024-09-23 12:37:16 +0200

    Outsource backend code into backend client; Fetch if  mapeditor editor opens

--- frontend/BackendClient/BackendClient.js
+++ frontend/BackendClient/BackendClient.js
@@ -292,4 +292,16 @@ export class BackendClient {
     }
     return await response.text();
   }
+
+   /**
+   * Fetches the list of assets from the server.
+   * @returns {Promise<Array<String>>}
+   */
+   static async get_assets() {
+    const response = await fetch('http://localhost:3000/sprites');
+    if (!response.ok) {
+      throw new Error(`Failed to fetch assets: ${response.statusText}`);
+    }
+    return await response.json();
+  }
 }
