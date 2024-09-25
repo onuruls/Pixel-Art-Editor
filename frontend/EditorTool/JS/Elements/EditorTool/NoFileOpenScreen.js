8c0622b9a00ff7f7eef9241825313f6b02c36296
onuruls <onur-uls@hotmail.com>
2024-09-25 18:10:25 +0200

    redesign: noFileScreen and TopMenu, add renameFileFunction in TopMenu

--- frontend/EditorTool/JS/Elements/EditorTool/NoFileOpenScreen.js
+++ frontend/EditorTool/JS/Elements/EditorTool/NoFileOpenScreen.js
@@ -10,41 +10,37 @@ export class NoFileOpenScreen extends HTMLElement {
     this.editor_tool = editor_tool;
     this.create_png_bind = this.create_new_file.bind(this, "png");
     this.create_tmx_bind = this.create_new_file.bind(this, "tmx");
-    this.create_sprite_file_button = null;
-    this.create_map_file_button = null;
-    this.css_link = document.createElement("link");
-    this.css_link.setAttribute(
-      "href",
-      "../EditorTool/CSS/Elements/NoFileOpenScreen.css"
-    );
-    this.css_link.setAttribute("rel", "stylesheet");
-    this.css_link.setAttribute("type", "text/css");
-    this.appendChild(this.css_link);
     this.init();
   }
 
-  /**
-   * Initializes the component.
-   */
   init() {
     this.classList.add("no-file-open-screen");
+
+    const css_link = document.createElement("link");
+    css_link.href = "../EditorTool/CSS/Elements/NoFileOpenScreen.css";
+    css_link.rel = "stylesheet";
+    css_link.type = "text/css";
+    this.appendChild(css_link);
+
     const message = document.createElement("p");
     message.textContent = "Please open a file to load the respective editor.";
+
     this.create_sprite_file_button = document.createElement("button");
     this.create_sprite_file_button.textContent = "Create New Sprite File";
+
     this.create_map_file_button = document.createElement("button");
     this.create_map_file_button.textContent = "Create New Map File";
+
     const button_container = document.createElement("div");
     button_container.classList.add("button-container");
-    button_container.appendChild(this.create_sprite_file_button);
-    button_container.appendChild(this.create_map_file_button);
-    this.appendChild(message);
-    this.appendChild(button_container);
+    button_container.append(
+      this.create_sprite_file_button,
+      this.create_map_file_button
+    );
+
+    this.append(message, button_container);
   }
 
-  /**
-   * Called when Element is connected to the DOM
-   */
   connectedCallback() {
     this.create_sprite_file_button.addEventListener(
       "click",
@@ -53,9 +49,6 @@ export class NoFileOpenScreen extends HTMLElement {
     this.create_map_file_button.addEventListener("click", this.create_tmx_bind);
   }
 
-  /**
-   * Called when Element is disconnnected from the DOM
-   */
   disconnectedCallback() {
     this.create_sprite_file_button.removeEventListener(
       "click",
@@ -82,15 +75,6 @@ export class NoFileOpenScreen extends HTMLElement {
           );
 
         this.editor_tool.set_active_file(new_file);
-
-        if (file_type === "png") {
-          this.editor_tool.sprite_editor.handle_loaded_matrix(
-            new_file.data.frames[0].matrix
-          );
-        } else if (file_type === "tmx") {
-          console.log("Not implemented yet");
-        }
-
         this.remove();
       } catch (error) {
         console.error("Error creating file:", error);
--- frontend/EditorTool/JS/Elements/EditorTool/TopMenu.js
+++ frontend/EditorTool/JS/Elements/EditorTool/TopMenu.js
@@ -6,114 +6,137 @@ import { EditorTool } from "../EditorTool.js";
  */
 export class TopMenu extends HTMLElement {
   /**
-   *
    * @param {EditorTool} editor_tool
    */
   constructor(editor_tool) {
     super();
     this.classList.add("top-menu");
     this.editor_tool = editor_tool;
+
     this.project_name = "Untitled Project";
-    this.project_container = null;
-    this.project_name_element = null;
-    this.file_container = null;
-    this.file_name_element = null;
-    this.input_container = this.create_input_container();
-    this.show_input_field_bind = this.show_input_field.bind(this);
-    this.keydown_bind = this.keydown.bind(this);
-    this.rename_bind = this.rename_project.bind(this, this.input_container);
-    this.remove_input_bind = this.remove_input.bind(this);
+    this.file_name = "-";
+
+    this.show_project_input_bind = this.show_project_input.bind(this);
+    this.project_keydown_bind = this.project_keydown.bind(this);
+    this.rename_project_bind = this.rename_project.bind(this);
+    this.remove_project_input_bind = this.remove_project_input.bind(this);
+
+    this.show_file_input_bind = this.show_file_input.bind(this);
+    this.file_keydown_bind = this.file_keydown.bind(this);
+    this.rename_file_bind = this.rename_file.bind(this);
+    this.remove_file_input_bind = this.remove_file_input.bind(this);
+
     this.init();
   }
 
   init() {
-    const file_container = this.create_file_container();
-    this.appendChild(file_container);
     const project_container = this.create_project_container();
-    this.appendChild(project_container);
+    const file_container = this.create_file_container();
+    this.append(project_container, file_container);
   }
 
-  connectedCallback() {
-    this.input.addEventListener("keydown", this.keydown_bind);
-    this.submit_icon.addEventListener("click", this.rename_bind);
-    this.cancel_icon.addEventListener("click", this.remove_input_bind);
-  }
-
-  disconnectedCallback() {
-    this.input.removeEventListener("keydown", this.keydown_bind);
-    this.submit_icon.removeEventListener("click", this.rename_bind);
-    this.cancel_icon.removeEventListener("click", this.remove_input_bind);
-  }
+  // -------------------- Project Rename Methods --------------------
 
   /**
-   * Handles the keydown events
-   * @param {Event} event
+   * Handles keydown events in the project input field
+   * @param {KeyboardEvent} event
    */
-  keydown(e) {
-    if (e.key === "Enter") {
-      this.rename_project(this.input_container);
-    } else if (e.key === "Escape") {
-      this.input_container.replaceWith(this.project_label);
+  project_keydown(event) {
+    switch (event.key) {
+      case "Enter":
+        this.rename_project();
+        break;
+      case "Escape":
+        this.remove_project_input();
+        break;
     }
   }
 
   /**
-   * Removes the input
+   * Removes the project input field and restores the project name display
    */
-  remove_input() {
-    this.input_container.replaceWith(this.project_label);
+  remove_project_input() {
+    this.project_input_container.replaceWith(this.project_name_element);
   }
 
   /**
-   * @returns {HTMLDivElement}
-   * */
-  create_file_container() {
-    this.file_container = document.createElement("div");
-    this.file_container.classList.add("file-container");
+   * Shows the input field for renaming the project
+   */
+  show_project_input() {
+    if (!this.project_input_container) {
+      this.project_input_container = this.create_project_input_container();
+    } else {
+      this.project_input.value = this.project_name;
+    }
 
-    const file_label = document.createElement("span");
-    file_label.textContent = "File: ";
-    this.file_name_element = document.createElement("span");
-    this.file_name_element.textContent =
-      this.editor_tool.active_file?.name || "Untitled File";
-
-    this.file_container.appendChild(file_label);
-    this.file_container.appendChild(this.file_name_element);
-
-    return this.file_container;
+    this.project_name_element.replaceWith(this.project_input_container);
+    this.project_input.focus();
   }
 
   /**
+   * Renames the project based on the input value
+   */
+  async rename_project() {
+    const new_name = this.project_input.value.trim();
+
+    if (new_name && new_name !== this.project_name) {
+      try {
+        await BackendClient.rename_project(
+          this.editor_tool.project.id,
+          new_name
+        );
+        this.editor_tool.project.name = new_name;
+        this.update_project_name(new_name);
+      } catch (error) {
+        console.error("Error renaming project:", error);
+      }
+    }
+    this.remove_project_input();
+  }
+
+  /**
+   * Creates the input container for renaming the project
    * @returns {HTMLDivElement}
-   * */
-  create_project_container() {
-    this.project_container = document.createElement("div");
-    this.project_container.classList.add("project-container");
+   */
+  create_project_input_container() {
+    const input_container = document.createElement("div");
+    input_container.classList.add("input-container");
 
-    this.project_label = document.createElement("span");
-    this.project_label.textContent = "Project: ";
+    this.project_input = document.createElement("input");
+    this.project_input.type = "text";
+    this.project_input.classList.add("rename-input");
+    this.project_input.value = this.project_name;
 
-    this.project_name = this.editor_tool.project?.name || this.project_name;
-    this.project_name_element = document.createElement("span");
-    this.project_name_element.textContent = this.project_name;
-
-    this.project_container.appendChild(this.project_label);
-    this.project_container.appendChild(this.project_name_element);
-
-    this.project_name_element.addEventListener(
-      "click",
-      this.show_input_field_bind
+    this.project_submit_icon = document.createElement("i");
+    this.project_submit_icon.classList.add(
+      "fa-solid",
+      "fa-check",
+      "submit-icon"
     );
 
-    return this.project_container;
-  }
+    this.project_cancel_icon = document.createElement("i");
+    this.project_cancel_icon.classList.add(
+      "fa-solid",
+      "fa-xmark",
+      "cancel-icon"
+    );
 
-  /**
-   * Updates the file name displayed in the file container
-   * @param {string} file_name
-   */
-  update_file_name(file_name) {
-    this.file_name_element.textContent = file_name || "Untitled File";
+    this.project_input.addEventListener("keydown", this.project_keydown_bind);
+    this.project_submit_icon.addEventListener(
+      "click",
+      this.rename_project_bind
+    );
+    this.project_cancel_icon.addEventListener(
+      "click",
+      this.remove_project_input_bind
+    );
+
+    input_container.append(
+      this.project_input,
+      this.project_submit_icon,
+      this.project_cancel_icon
+    );
+    return input_container;
   }
 
   /**
@@ -123,68 +146,164 @@ export class TopMenu extends HTMLElement {
   update_project_name(project_name) {
     this.project_name = project_name;
     this.project_name_element.textContent = project_name;
-    const input = this.input_container.querySelector("input");
-    input.value = project_name;
+    if (this.project_input) {
+      this.project_input.value = project_name;
+    }
+  }
+
+  // -------------------- File Rename Methods --------------------
+
+  /**
+   * Handles keydown events in the file input field
+   * @param {KeyboardEvent} event
+   */
+  file_keydown(event) {
+    switch (event.key) {
+      case "Enter":
+        this.rename_file();
+        break;
+      case "Escape":
+        this.remove_file_input();
+        break;
+    }
   }
 
   /**
-   * Creates the input container for the project rename
+   * Removes the file input field and restores the file name display
+   */
+  remove_file_input() {
+    this.file_input_container.replaceWith(this.file_name_element);
+  }
+
+  /**
+   * Shows the input field for renaming the file
+   */
+  show_file_input() {
+    if (this.editor_tool.active_file) {
+      if (!this.file_input_container) {
+        this.file_input_container = this.create_file_input_container();
+      } else {
+        this.file_input.value = this.file_name;
+      }
+
+      this.file_name_element.replaceWith(this.file_input_container);
+      this.file_input.focus();
+    }
+  }
+
+  /**
+   * Renames the file based on the input value
+   */
+  async rename_file() {
+    const new_name = this.file_input.value.trim();
+
+    if (new_name && new_name !== this.file_name) {
+      try {
+        await BackendClient.rename_file_by_id(
+          this.editor_tool.active_file.id,
+          new_name
+        );
+        this.editor_tool.active_file.name = new_name;
+        this.update_file_name(new_name);
+      } catch (error) {
+        console.error("Error renaming file:", error);
+      }
+    }
+    this.editor_tool.file_area.file_view.rebuild_view();
+    this.remove_file_input();
+  }
+
+  /**
+   * Creates the input container for renaming the file
    * @returns {HTMLDivElement}
    */
-  create_input_container() {
+  create_file_input_container() {
     const input_container = document.createElement("div");
     input_container.classList.add("input-container");
 
-    this.input = document.createElement("input");
-    this.input.type = "text";
-    this.input.classList.add("rename-input");
-    this.input.value = this.project_name;
+    this.file_input = document.createElement("input");
+    this.file_input.type = "text";
+    this.file_input.classList.add("rename-input");
+    this.file_input.value = this.file_name;
 
-    this.submit_icon = document.createElement("i");
-    this.submit_icon.classList.add("fa-solid", "fa-check", "submit-icon");
+    this.file_submit_icon = document.createElement("i");
+    this.file_submit_icon.classList.add("fa-solid", "fa-check", "submit-icon");
 
-    this.cancel_icon = document.createElement("i");
-    this.cancel_icon.classList.add("fa-solid", "fa-xmark", "cancel-icon");
+    this.file_cancel_icon = document.createElement("i");
+    this.file_cancel_icon.classList.add("fa-solid", "fa-xmark", "cancel-icon");
 
-    input_container.appendChild(this.input);
-    input_container.appendChild(this.submit_icon);
-    input_container.appendChild(this.cancel_icon);
+    this.file_input.addEventListener("keydown", this.file_keydown_bind);
+    this.file_submit_icon.addEventListener("click", this.rename_file_bind);
+    this.file_cancel_icon.addEventListener(
+      "click",
+      this.remove_file_input_bind
+    );
+
+    input_container.append(
+      this.file_input,
+      this.file_submit_icon,
+      this.file_cancel_icon
+    );
     return input_container;
   }
 
   /**
-   * Shows the input field for renaming the project
+   * Updates the file name displayed in the file container
+   * @param {string} file_name
    */
-  show_input_field() {
-    this.project_label =
-      this.project_container.querySelector("span:last-child");
-    this.project_label.replaceWith(this.input_container);
-    this.input.focus();
+  update_file_name(file_name) {
+    this.file_name = file_name;
+    this.file_name_element.textContent = file_name;
+    if (this.file_input) {
+      this.file_input.value = file_name;
+    }
+  }
+
+  // -------------------- UI Creation Methods --------------------
+
+  /**
+   * Creates the project container element
+   * @returns {HTMLDivElement}
+   */
+  create_project_container() {
+    const project_container = document.createElement("div");
+    project_container.classList.add("project-container");
+
+    const project_label = document.createElement("span");
+    project_label.textContent = "Project: ";
+
+    this.project_name = this.editor_tool.project?.name || this.project_name;
+    this.project_name_element = document.createElement("span");
+    this.project_name_element.textContent = this.project_name;
+
+    this.project_name_element.addEventListener(
+      "click",
+      this.show_project_input_bind
+    );
+
+    project_container.append(project_label, this.project_name_element);
+    return project_container;
   }
 
   /**
-   * Renames the project
-   * @param {HTMLDivElement} input_container
-   * */
-  async rename_project(input_container) {
-    const input = input_container.querySelector("input");
-    const new_name = input.value.trim();
+   * Creates the file container element
+   * @returns {HTMLDivElement}
+   */
+  create_file_container() {
+    const file_container = document.createElement("div");
+    file_container.classList.add("file-container");
 
-    if (new_name && new_name !== this.project_name) {
-      try {
-        await BackendClient.rename_project(
-          this.editor_tool.project.id,
-          new_name
-        );
-        this.editor_tool.project.name = new_name;
-        this.project_name = new_name;
-      } catch (error) {
-        console.error("Error renaming project:", error);
-      }
-    }
+    const file_label = document.createElement("span");
+    file_label.textContent = "File: ";
 
-    this.project_name_element.textContent = this.project_name;
-    input_container.replaceWith(this.project_name_element);
+    this.file_name = this.editor_tool.active_file?.name || this.file_name;
+    this.file_name_element = document.createElement("span");
+    this.file_name_element.textContent = this.file_name;
+
+    this.file_name_element.addEventListener("click", this.show_file_input_bind);
+
+    file_container.append(file_label, this.file_name_element);
+    return file_container;
   }
 }
 
